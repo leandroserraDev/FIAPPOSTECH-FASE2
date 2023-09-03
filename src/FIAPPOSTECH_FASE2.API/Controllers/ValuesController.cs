81a7310a21e96c79e6302b71a426b23e79f4ee61
Leandro Serra <leandroserra.dba@hotmail.com>
2023-09-03 15:45:49 -0300

    .

---
+++ src/FIAPPOSTECH_FASE2.API/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIAPPOSTECH_FASE2.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
